b75c9efcc18d628a4c580584aa03a6c14f55bb83
DavidYedidya <89817880+DavidYedidya@users.noreply.github.com>
2021-11-24 00:14:31 +0800

    Add files via upload

---
+++ Experiment 5/src/experiment5/Singleton.java
@@ -0,0 +1,27 @@
+package Experiment5;
+//Naïve Singleton (single-threaded)
+/*
+ * It’s pretty easy to implement a sloppy Singleton. 
+ * You just need to hide the constructor and implement a static creation method.
+ * */
+public final class Singleton {
+    private static Singleton instance;
+    public String value;
+
+    private Singleton(String value) {
+        // The following code emulates slow initialization.
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+        if (instance == null) {
+            instance = new Singleton(value);
+        }
+        return instance;
+    }
+}
